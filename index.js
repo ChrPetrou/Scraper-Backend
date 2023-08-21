417d153737ac9521dd3f6a3dec3547011f577614
ChristosPetrou <c.petrou@tiomarkets.com>
2023-08-21 10:59:49 +0300

    something is happening

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const main = async () => {
   console.log("Connected!");
 
   app.listen(PORT, () => {
-    generic();
+    setInterval(generic, 60);
 
     console.log(`Server started on port http://localhost:${PORT}`);
   });
@@ -26,18 +26,64 @@ const main = async () => {
 const generic = async () => {
   const browser = await puppeteer.launch({
     headless: false,
+    args: ["--window-size=1920,1040"], //open page with this dimensions
+  });
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1080, height: 1024 });
+  await page.goto("https://www.tradingview.com/symbols/EURUSD/?exchange=FX");
+
+  console.log("wait");
+
+  await page.waitForSelector(
+    ".container-pAUXADuj.containerWithButton-pAUXADuj h1"
+  );
+
+  const title = await page.$eval(
+    ".container-pAUXADuj.containerWithButton-pAUXADuj h1",
+    (el) => {
+      return el.innerText;
+    }
+  );
+  console.log(title);
+
+  await page.waitForSelector(".lastContainer-JWoJqCpY span");
+
+  const result = await page.$$eval(".lastContainer-JWoJqCpY span", (el) => {
+    return el[0].innerText;
+  });
+  console.log(result);
+
+  await page.waitForSelector(".block-sjmalUvv button");
+
+  const prog = await page.$$eval(".block-sjmalUvv button", (el) => {
+    return Array.from(el).map((x) => x.innerText.replace("\n", "- "));
+  });
+  console.log(prog);
+};
+
+const generic2 = async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: ["--window-size=1920,1040"], //open page with this dimensions
+    defaultViewport: null,
   });
 
   const page = await browser.newPage();
   await page.goto("https://www.tradingview.com/symbols/EURUSD/?exchange=FX");
 
   console.log("wait");
-  await page.waitForSelector(".js-symbol-last");
-  console.log("ddadas");
-  await page.$$eval("span.js-symbol-last", (el) => {
-    console.log("found");
-    console.log(el[0]);
+  const names = await page.evaluate(() => {
+    // return Array.from(document.querySelectorAll(".info strong")).map(
+    //   (x) => x.textContent
+    // );
+    return Array.from(
+      document.querySelectorAll(".lastContainer-JWoJqCpY span")
+    ).map((x) => x.textContent);
+    // return document
+    //   .querySelectorAll(".js-symbol-header-ticker .quote-ticker-inited span")
+    //   .values((x) => x.textContent);
   });
+  console.log(names);
 
   console.log("ended");
   //   await page.screenshot({ path: "amazing.png" });
