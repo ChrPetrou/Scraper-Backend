34f4ca3002f1e686dad41db670178e3c1c172932
ChristosPetrou <c.petrou@tiomarkets.com>
2023-08-22 17:32:13 +0300

    added service for ols

--- index.js
+++ index.js
@@ -7,18 +7,47 @@ const mongoose = require("mongoose");
 var CronJob = require("cron").CronJob;
 var _ = require("lodash");
 const symbols = require("./routes/symbols.js");
+const tradingViewService = require("./service/tradingView.service.js");
+const symbolModel = require("./models/symbolModel");
 
 const PORT = 4000;
 
 const main = async () => {
-  var job = new CronJob(
+  new CronJob(
     "*/10 * * * * *",
-    generic,
+    async () => {
+      console.log("GETTING RATES");
+      const result = await tradingViewService.getRatesForSymbols([
+        "EURUSD",
+        "GBPUSD",
+        "USDJPY",
+      ]);
+
+      console.log(result);
+    },
     null,
     true,
     "America/Los_Angeles"
   );
-  job.start();
+
+  //runs everyday at 6 am
+  new CronJob(
+    "0 0 6 * * *",
+    async () => {
+      console.log("GETTING Performance");
+      const result = await tradingViewService.getPerformanceForSymbols([
+        "EURUSD",
+        "GBPUSD",
+        "USDJPY",
+      ]);
+
+      console.log(result);
+    },
+    null,
+    true,
+    "Europe/Athens"
+  );
+
   const app = express();
   app.use(express.json());
 
@@ -31,82 +60,6 @@ const main = async () => {
   app.listen(PORT, () => {
     console.log(`Server started on port http://localhost:${PORT}`);
   });
-};
-
-const generic = async () => {
-  const browser = await puppeteer.launch({
-    headless: "new",
-    args: ["--window-size=1920,1040"], //open page with this dimensions
-    defaultViewport: null,
-  });
-  const [page1, page2, page3] = await Promise.all([
-    browser.newPage(),
-    browser.newPage(),
-    browser.newPage(),
-  ]);
-
-  const results = await Promise.all([
-    get(
-      page1,
-      "https://www.tradingview.com/symbols/EURUSD/?exchange=FX",
-      "EURUSD"
-    ),
-    get(
-      page2,
-      "https://www.tradingview.com/symbols/GBPUSD/?exchange=FX",
-      "GBPUSD"
-    ),
-    get(
-      page3,
-      "https://www.tradingview.com/symbols/USDJPY/?exchange=FX",
-      "USDJPY"
-    ),
-  ]);
-
-  console.log(results[0]);
-
-  async function get(page, url, symbol) {
-    await page.setViewport({ width: 1080, height: 1024 });
-    await page.goto(url);
-
-    // console.log("wait");
-
-    await page.waitForSelector(
-      ".container-pAUXADuj.containerWithButton-pAUXADuj h1"
-    );
-
-    const title = await page.$eval(
-      ".container-pAUXADuj.containerWithButton-pAUXADuj h1",
-      (el) => {
-        return el.innerText;
-      }
-    );
-    // console.log(title);
-
-    await page.waitForSelector(".lastContainer-JWoJqCpY span");
-
-    const result = await page.$$eval(".lastContainer-JWoJqCpY span", (el) => {
-      return el[0].innerText;
-    });
-    // console.log(result);
-
-    await page.waitForSelector(".block-sjmalUvv button");
-
-    let prog = await page.$$eval(".block-sjmalUvv button", (el) => {
-      return Array.from(el).map((x) => x.innerText);
-    });
-    prog = prog.map((x) => {
-      return [
-        _.camelCase(x.split("\n")[0].split(" ").reverse().join("")),
-        parseFloat(x.split("\n")[1].replace("−", "-").split("%")[0] / 100),
-      ];
-    });
-    prog = Object.fromEntries(prog);
-
-    return { title, result: parseFloat(result), ...prog, symbol };
-  }
-
-  await browser.close();
 };
 
 main();
---
+++ service/tradingView.service.js
@@ -0,0 +1,100 @@
+var puppeteer = require("puppeteer");
+var _ = require("lodash");
+
+/**
+ *
+ * @param {puppeteer.Browser} browser
+ * @param {*} url
+ * @param {*} symbol
+ * @returns
+ */
+async function getRates(browser, url, symbol) {
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1080, height: 1024 });
+  await page.goto(url);
+
+  await page.waitForSelector(
+    ".container-pAUXADuj.containerWithButton-pAUXADuj h1"
+  );
+
+  const title = await page.$eval(
+    ".container-pAUXADuj.containerWithButton-pAUXADuj h1",
+    (el) => {
+      return el.innerText;
+    }
+  );
+
+  await page.waitForSelector(".lastContainer-JWoJqCpY span");
+
+  const rate = await page.$$eval(".lastContainer-JWoJqCpY span", (el) => {
+    return el[0].innerText;
+  });
+
+  await page.waitForSelector(".block-sjmalUvv button");
+
+  let prog = await page.$$eval(".block-sjmalUvv button", (el) => {
+    return Array.from(el).map((x) => x.innerText);
+  });
+  prog = prog.map((x) => {
+    return [
+      _.camelCase(x.split("\n")[0].split(" ").reverse().join("")),
+      parseFloat(x.split("\n")[1].replace("−", "-").split("%")[0] / 100),
+    ];
+  });
+  prog = Object.fromEntries(prog);
+
+  return { title, rate: parseFloat(rate), ...prog, symbol };
+}
+
+const tradingViewService = {
+  getRatesForSymbols: async (symbols) => {
+    const browser = await puppeteer.launch({
+      headless: "new",
+      args: ["--window-size=1920,1040"], //open page with this dimensions
+      defaultViewport: null,
+    });
+
+    const results = await Promise.all(
+      symbols.map(async (esymbol, index) => {
+        const { rate, symbol } = await getRates(
+          browser,
+          `https://www.tradingview.com/symbols/${esymbol}/?exchange=FX`,
+          esymbol
+        );
+        return { rate, symbol };
+      })
+    );
+
+    //   console.log(results);
+    await browser.close();
+    return results;
+  },
+  getPerformanceForSymbols: async (symbols) => {
+    const browser = await puppeteer.launch({
+      headless: "new",
+      args: ["--window-size=1920,1040"], //open page with this dimensions
+      defaultViewport: null,
+    });
+
+    const [page1, page2, page3] = await Promise.all([
+      browser.newPage(),
+      browser.newPage(),
+      browser.newPage(),
+    ]);
+
+    const results = await Promise.all(
+      symbols.map(async (symbol, index) => {
+        return getRates(
+          browser,
+          `https://www.tradingview.com/symbols/${symbol}/?exchange=FX`,
+          symbol
+        );
+      })
+    );
+
+    await browser.close();
+    return results;
+  },
+};
+
+module.exports = tradingViewService;
